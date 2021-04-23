8b053611ccfb91af340e68f9026f2faa8d83d432
akram.sahbi@esprit.tn <akram.sahbi@esprit.tn>
2021-04-23 21:20:10 +0100

    cracking caesar cipher

--- main/caesar/caesar.java
+++ main/caesar/caesar.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class caesar {
     private static final int ASCII_LENGTH = 256;
+    private static final String DELIMITER = "####################################################################################";
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println(DELIMITER);
         System.out.println("Enter text to encrypt using ceasar cipher:");
         String plaintext = scanner.nextLine();
         System.out.println("Enter key numeric value:");
@@ -17,8 +19,13 @@ public class caesar {
         System.out.println("Encrypted using ceasar cipher: " + ciphertext);
         String decryptedText = decrypt(ciphertext, key);
         System.out.println("decrypted back using ceasar cipher: " + decryptedText);
+        crackBruteForce(ciphertext);
+        System.out.println(DELIMITER);
+        crackFrequencyAnalysis(ciphertext);
+        System.out.println(DELIMITER);
     }
 
+    //encrypt plain text using caesar cipher
     private static String encrypt(String plaintext, int key) {
         String ciphertext = "";
         for (int i = 0; i < plaintext.length(); i++) {
@@ -32,6 +39,7 @@ public class caesar {
         return ciphertext;
     }
 
+    //decrypt plain text using caesar cipher
     private static String decrypt(String ciphertext, int key) {
         String plaintext = "";
         for (int i = 0; i < ciphertext.length(); i++) {
@@ -43,5 +51,52 @@ public class caesar {
         }
 
         return plaintext;
+    }
+
+    //cracking caesar cipher using brute force attack
+    private static void crackBruteForce(String ciphertext) {
+        System.out.println("#######     Cracking Caesar Cipher using brute force attack     #######");
+        for (int key = 1; key < ASCII_LENGTH; key++) {
+            String decryptedText = decrypt(ciphertext, key);
+            System.out.println("using key: " + key + " : " + decryptedText);
+        }
+    }
+
+    //cracking caesar cipher using frequency analysis attack
+    private static void crackFrequencyAnalysis(String ciphertext) {
+        int[] keyScores = new int[ASCII_LENGTH];
+        System.out.println("#######     Cracking Caesar Cipher using frequency analysis attack      #######");
+        for (int key = 1; key < ASCII_LENGTH; key++) {
+            String decryptedText = decrypt(ciphertext, key);
+            keyScores[key] = calculateFrequencyAnalisisScore(decryptedText);
+        }
+        int key = maxFrequencyKey(keyScores);
+        System.out.println("The key found using frequency analysis attack is: " + key);
+        System.out.println("message found: " + decrypt(ciphertext, key));
+        System.out.println(DELIMITER);
+    }
+
+    private static int calculateFrequencyAnalisisScore(String text) {
+        String mostFrequentCharactersInEnglishLanguage = "aAeEiIoOnNtT";
+        int frequencyScore = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (mostFrequentCharactersInEnglishLanguage.contains(text.charAt(i)+"")) {
+                frequencyScore++;
+            }
+        }
+        return frequencyScore;
+    }
+
+    private static int maxFrequencyKey(int[] frequencyArray) {
+        int max = -1;
+        int index = -1;
+
+        for (int i = 0; i < frequencyArray.length; i++) {
+            if (frequencyArray[i] > max) {
+                max = frequencyArray[i];
+                index = i;
+            }
+        }
+        return index;
     }
 }
